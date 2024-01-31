5e0e34f788930b74f7c038b6b4fbd8732582c20a
eddwardmessias <eddwardmessias@hotmail.com>
2024-01-30 22:43:42 -0300

    Hello ajustada

--- backend/src/main/java/com/dev/backend/controller/Hello.java
+++ backend/src/main/java/com/dev/backend/controller/Hello.java
@@ -17,27 +17,5 @@ public class Hello {
     public String hello(){
         return "Olá mundo Spring" + new Date();
     }
-    @Autowired
-    private EstadoService estadoService;
 
-    @GetMapping("/dois")
-    public List<Estado> buscarTodos(){
-        return estadoService.buscarTodos();
-    }
-
-    @PostMapping("/")
-    public Estado inserir(@RequestBody Estado estado){
-        return estadoService.inserir(estado);
-    }
-
-    @PutMapping("/")
-    public Estado alterar(@RequestBody Estado estado){
-        return estadoService.alterar(estado);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluir(@PathVariable Long id){
-        estadoService.excluir(id);
-        return ResponseEntity.ok().build();
-    }
 }
