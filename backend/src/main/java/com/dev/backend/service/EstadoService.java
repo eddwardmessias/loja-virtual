53b61d4381db470b7aa381530e09e198c43a4623
eddwardmessias <eddwardmessias@hotmail.com>
2024-01-31 16:34:47 -0300

    Envio Crud Estado

---
+++ backend/src/main/java/com/dev/backend/service/EstadoService.java
@@ -0,0 +1,37 @@
+package com.dev.backend.service;
+
+import com.dev.backend.EstadoRepository;
+import com.dev.backend.entity.Estado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class EstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> buscarTodos(){
+        return  estadoRepository.findAll();
+    }
+    public Estado inserir(Estado estado){
+        estado.setDataCriacao(new Date());
+        Estado estadoNovo = estadoRepository.saveAndFlush(estado);
+        return estadoNovo;
+    }
+
+    public Estado alterar(Estado estado){
+        estado.setDataAtualizacao(new Date());
+        Estado estadoNovo = estadoRepository.saveAndFlush(estado);
+        return estadoNovo;
+    }
+
+    public void excluir(Long id){
+        Estado estado = estadoRepository.findById(id).get();
+        estadoRepository.delete(estado);
+    }
+
+}
